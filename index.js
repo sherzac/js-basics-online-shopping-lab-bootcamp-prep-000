952c218cfaf6ae0f2fd3649abc0895e99f22ff19
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-09 16:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,12 @@ function total() {
 }
 
 function removeFromCart(item) {
-  
+  for(let i = 0; i < cart.length; i++) {
+    if(cart[i].itemName === item) {
+      cart.splice(i, 1);
+    }
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
