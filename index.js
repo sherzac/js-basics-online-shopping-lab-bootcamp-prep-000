ce2d05caa7aae87fca655f0a909648bb93121b0a
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-09 14:50:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-<<<<<<< HEAD
   var cartItems = []
   if (cart.length !== 0) {
    for(var i = 0; i < cart.length; i++) {
@@ -28,28 +27,6 @@ function viewCart() {
     } else {
         return 'Your shopping cart is empty.';
     }
-=======
-  let cartItems = [];
-
-  if (cart.length === 0) {
-    return "Your shopping cart is empty.";
-  
-  } else if (cart.length === 1) {
-    let oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`;
-    return oneItem;
-
-  } else if (cart.length === 2) {
-    let twoItems = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`;
-    console.log(twoItems);
-
-  } else {
-    for (let i = 0; i < cart.length - 1; i++) {
-      cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
-    }
-    let threeOrMoreItems = `In your cart, you have ${cartItems}, and ${Object.keys(cart[cart.length - 1])} at $${cart[cart.length - 1][Object.keys(cart[cart.length - 1])]}`;
-    console.log(threeOrMoreItems);
-  }
->>>>>>> c6db1b344520ceae49e183fba0f5ebf792de04b5
 }
 
 function total() {
